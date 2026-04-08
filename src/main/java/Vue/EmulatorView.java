188630aeb1ae039dba006e9a9a64575ed4a08dba
Lain <crystaltrd@riseup.net>
2026-04-08 02:02:33 +0200

    init: Tilemap display

--- src/main/java/Vue/EmulatorView.java
+++ src/main/java/Vue/EmulatorView.java
@@ -6,6 +6,7 @@ import Model.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.security.cert.X509Certificate;
 
 public class EmulatorView extends JFrame {
     public final EmulationContext ctx;
@@ -16,11 +17,16 @@ public class EmulatorView extends JFrame {
     public JTextArea debugScreen;
     JToolBar toolBar;
     JPanel gameCanvas;
-    JPanel tileMap;
+    TileMapPanel tileMap;
     JPanel registersPanel;
+    Color[] palette = {
+            new Color(0xaaaaaa),
+            new Color(0x9bbc0f),
+            new Color(0x306230),
+            new Color(0x0f380f)
+    };
+    int scale = 2;
 
-    // Registers
-    //JTextField regAField;
     public EmulatorView(EmulationContext ctx) {
         this.ctx = ctx;
         setTitle("Gamegirl Emu - " + ctx.cartridge.getTitle());
@@ -33,6 +39,8 @@ public class EmulatorView extends JFrame {
         debugScreen.setMinimumSize(new Dimension(450, 60));
         debugScreen.setMaximumSize(new Dimension(450, 90));
         debugScreen.setEditable(false);
+        tileMap = new TileMapPanel(this);
+        getContentPane().add(tileMap, BorderLayout.EAST);
         getContentPane().add(toolBar, BorderLayout.NORTH);
         getContentPane().add(registersPanel, BorderLayout.WEST);
         getContentPane().add(debugScreen, BorderLayout.SOUTH);
@@ -75,5 +83,54 @@ public class EmulatorView extends JFrame {
         this.getContentPane().add(registersPanel, BorderLayout.WEST);
         pack();
         setVisible(true);
+    }
+}
+
+class TileMapPanel extends JPanel {
+    public EmulatorView parent;
+
+    public TileMapPanel(EmulatorView parent) {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        this.parent = parent;
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(27 * 8 * parent.scale, 16 * 8 * parent.scale);
+    }
+
+    public void drawTile(Graphics g, char addr, int tileNum, int x, int y) {
+        int rc_x = 0, rc_y = 0, rc_w = 0, rc_h = 0;
+        for (int tileY = 0; tileY < 16; tileY += 2) {
+            byte b1 = parent.ctx.bus_read((char) (addr + (tileNum * 16) + tileY));
+            byte b2 = parent.ctx.bus_read((char) (addr + (tileNum * 16) + tileY + 1));
+            for (int j = 7; j >= 0; j--) {
+                byte pixel = 0;
+                pixel = (byte) ((byte) (((b2 >> j) & 1) << 1) | (byte) (((b1 >> j) & 1)));
+                rc_x = x + ((7 - j) * parent.scale);
+                rc_y = y + ((tileY / 2) * parent.scale);
+                rc_w = parent.scale;
+                rc_h = parent.scale;
+                g.setColor(parent.palette[pixel]);
+                g.fillRect(rc_x, rc_y, rc_w, rc_h);
+            }
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        int tilenum = 0;
+        int xDraw = 0;
+        int yDraw = 0;
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 27 * 8 * parent.scale, 18 * 8 * parent.scale);
+        for (int y = 0; y < 16; y++) {
+            for (int x = 0; x < 24; x++) {
+                drawTile(g, (char) 0x8000, tilenum, xDraw + (x * parent.scale), yDraw + (y * parent.scale));
+                xDraw += (8 * parent.scale);
+                tilenum++;
+            }
+            yDraw += (8 * parent.scale);
+            xDraw = 0;
+        }
     }
 }
